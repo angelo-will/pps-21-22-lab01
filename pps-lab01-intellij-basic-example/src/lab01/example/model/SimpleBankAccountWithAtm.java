3e8188a8239d598bc7c4644f7cffdddf0375bf87
Angelo Tinti <angelo.tinti.30@gmail.com>
2022-02-28 12:18:58 +0100

    Modify SimpleBankAccountWithAtm class to reuse abstract bank account class

--- pps-lab01-intellij-basic-example/src/lab01/example/model/SimpleBankAccountWithAtm.java
+++ pps-lab01-intellij-basic-example/src/lab01/example/model/SimpleBankAccountWithAtm.java
@@ -5,58 +5,18 @@ package lab01.example.model;
  * In particular, a Simple Bank Account allows always the deposit
  * while the withdrawal is allowed only if the balance greater or equal the withdrawal amount
  */
-public class SimpleBankAccountWithAtm implements BankAccount {
+public class SimpleBankAccountWithAtm extends AbstractBankAccount implements BankAccount {
 
-    private static final double FEE = 1;
 
-    private double balance;
-    private final AccountHolder holder;
+    public static final int FEE_TRANSACTION = 1;
 
     public SimpleBankAccountWithAtm(final AccountHolder holder, final double balance) {
-        this.holder = holder;
-        this.balance = balance;
-    }
-    @Override
-    public AccountHolder getHolder(){
-        return this.holder;
+        super(balance, holder);
     }
 
     @Override
-    public double getBalance() {
-        return this.balance;
+    protected double getFee() {
+        return SimpleBankAccountWithAtm.FEE_TRANSACTION;
     }
 
-    @Override
-    public void deposit(final int userID, final double amount) {
-        if (checkUser(userID)) {
-            this.balance += amount;
-        }
-    }
-
-    public void depositWithAtm(final int userID, final double amount) {
-        if (checkUser(userID)) {
-            this.deposit(userID, amount-SimpleBankAccountWithAtm.FEE);
-        }
-    }
-
-    @Override
-    public void withdraw(final int userID, final double amount) {
-        if (checkUser(userID) && isWithdrawAllowed(amount)) {
-            this.balance -= amount;
-        }
-    }
-
-    public void withdrawWithAtm(final int userID, final double amount) {
-        if (checkUser(userID)) {
-            this.withdraw(userID,amount+SimpleBankAccountWithAtm.FEE);
-        }
-    }
-
-    private boolean isWithdrawAllowed(final double amount){
-        return this.balance >= amount;
-    }
-
-    private boolean checkUser(final int id) {
-        return this.holder.getId() == id;
-    }
 }
