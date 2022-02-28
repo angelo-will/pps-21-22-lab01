5d58f440a950ec1a3161cb4f780f15015e2a4582
Angelo Tinti <angelo.tinti.30@gmail.com>
2022-02-28 12:17:34 +0100

    Add abstrac class to implement bank account interface

---
+++ pps-lab01-intellij-basic-example/src/lab01/example/model/AbstractBankAccount.java
@@ -0,0 +1,54 @@
+package lab01.example.model;
+
+public abstract class AbstractBankAccount implements BankAccount{
+
+    protected final AccountHolder holder;
+    protected double balance;
+
+    public AbstractBankAccount(final double balance, final AccountHolder holder) {
+        this.balance = balance;
+        this.holder = holder;
+    }
+
+    public AccountHolder getHolder() {
+        return this.holder;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+
+    public void deposit(final int userID, final double amount) {
+        if (checkUser(userID)) {
+            this.balance += amount;
+        }
+    }
+
+    public void depositWithAtm(final int userID, final double amount) {
+        if (checkUser(userID)) {
+            this.deposit(userID, amount - this.getFee());
+        }
+    }
+
+    public void withdraw(final int userID, final double amount) {
+        if (checkUser(userID) && isWithdrawAllowed(amount)) {
+            this.balance -= amount;
+        }
+    }
+
+    public void withdrawWithAtm(final int userID, final double amount) {
+        if (checkUser(userID)) {
+            this.withdraw(userID, amount + this.getFee());
+        }
+    }
+
+    private boolean isWithdrawAllowed(final double amount) {
+        return this.balance >= amount;
+    }
+
+    protected abstract double getFee();
+
+    private boolean checkUser(final int id) {
+        return this.holder.getId() == id;
+    }
+}
