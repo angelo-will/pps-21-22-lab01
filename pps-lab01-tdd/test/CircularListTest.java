6303bb2c0fd36068c6fde3545a7265c15f194d18
Angelo Tinti <angelo.tinti.30@gmail.com>
2022-02-28 17:55:04 +0100

    Add beforeEach and size method test to CircularListTest

--- pps-lab01-tdd/test/CircularListTest.java
+++ pps-lab01-tdd/test/CircularListTest.java
@@ -1,14 +1,32 @@
+import lab01.tdd.CircularList;
+import lab01.tdd.CircularListImpl;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * The test suite for testing the CircularList implementation
  */
 public class CircularListTest {
 
-    //TODO: test implementation
+    CircularList circularList;
 
+    @BeforeEach
+    void beforeEach(){
+        circularList = new CircularListImpl();
+    }
+
+    @Test
+    void testSizeAddingOneElement(){
+        circularList.add(5);
+        assertEquals(1, circularList.size());
+    }
+
+
+    //TODO: test implementation
     @Disabled
     @Test public void testTodo(){
         Assertions.fail();
