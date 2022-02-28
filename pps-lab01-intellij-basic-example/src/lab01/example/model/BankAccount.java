7f2df186aa00fc81b8080cfcb57c70d5cc44adb9
Angelo Tinti <angelo.tinti.30@gmail.com>
2022-02-28 12:15:49 +0100

    Add withdraw with atm and deposit with atm methods to bank account interface

--- pps-lab01-intellij-basic-example/src/lab01/example/model/BankAccount.java
+++ pps-lab01-intellij-basic-example/src/lab01/example/model/BankAccount.java
@@ -26,6 +26,8 @@ public interface BankAccount {
      */
     void deposit(int userID, double amount);
 
+    void depositWithAtm(int userID, double amount);
+
     /**
      * Allows the withdrawal of an amount from the account, if the given userID corresponds to the register holder ID
      * of the bank account. This ID acts like an "identification token" .
@@ -34,4 +36,6 @@ public interface BankAccount {
      * @param amount the amount of the withdrawal
      */
     void withdraw(int userID, double amount);
+
+    void withdrawWithAtm(int userID, double amount);
 }
